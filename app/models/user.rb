eee7e855dd722b8823f3e66ab025a224d35d8cb4
kengo922 <kengo.matsuno.922@gmail.com>
2020-06-26 09:26:41 +0900

    v12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   # セーブする時、上のコードが走る。
   # downcase 大文字
@@ -13,13 +14,27 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   # 渡された文字列のハッシュ値を返す
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
 
 # :case_sensitive	→ 大文字と小文字を区別するか	デフォルト値はtrue
